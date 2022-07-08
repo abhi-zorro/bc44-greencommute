eee1a88de8c03cc6a7664f60ce3bf4b8beb0f94d
abhi-zorro <abhishek.bhemisetty@zemosolabs.com>
2022-07-08 14:10:11 +0530

    added test cases

--- src/components/atoms/InputField/index.test.tsx
+++ src/components/atoms/InputField/index.test.tsx
@@ -1,20 +1,39 @@
-
 import React from 'react';
-import {render, screen} from '@testing-library/react';
-
-import  InputComponent  from '.';
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import InputComponent from '.';
 
 describe('Atoms', () => {
-    test('renders input field', () => {
-        const handleClick = () => {
-            console.info('You clicked the Chip.');
-          };
-        
-          const handleDelete = () => {
-            console.info('You clicked the delete icon.');
-          };
-        render(<InputComponent placeholder={"Search by job location"} handleClick = {handleClick} handleDelete = {handleDelete}/>);
+  test('renders input field', () => {
+    const handleClick = () => {
+      console.info('You clicked the Chip.');
+    };
 
-        expect(screen.getByRole("combobox")).toBeInTheDocument();
-    })
+    const handleDelete = () => {
+      console.info('You clicked the delete icon.');
+    };
+    render(
+      <InputComponent placeholder={'Search by job location'} handleClick={handleClick} handleDelete={handleDelete} />
+    );
+
+    expect(screen.getByRole('combobox')).toBeInTheDocument();
+  });
+});
+
+describe('Input atom', () => {
+  it('renders correctly', () => {
+    const handleClick = () => {
+      console.info('You clicked the Chip.');
+    };
+
+    const handleDelete = () => {
+      console.info('You clicked the delete icon.');
+    };
+    const tree = renderer
+      .create(
+        <InputComponent placeholder={'Search by job location'} handleClick={handleClick} handleDelete={handleDelete} />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
